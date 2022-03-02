a9d2edec567ca38c0d472f032ef6678f97d2fcd6
Junaid Qureshi <Junaid.562000@gmail.com>
2022-03-02 10:14:58 +0530

    GET /tours

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
-import express from 'express';
-const app = express();
+const express = require('express');
+const fs = require('fs');
 const port = 3000;
+const app = express();
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours.json`)
+);
 
 app.get('/', (req, res) => {
-  res.send('<h1>This API is working</h1>');
+  res.json({
+    name: 'Natours API',
+  });
+});
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tours,
+    },
+  });
+});
+app.post('/', (req, res) => {
+  res.send('you can post to this route');
 });
 app.listen(port, () => console.log('Server started on port 3000'));
